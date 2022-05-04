054e1900b1c02b28eaa4431bc245dfaad4b4b40d
itays123 <itays2005@gmail.com>
2022-05-04 19:26:33 +0300

    FEATURE: IscoolFetchTask

---
+++ src/utils/AsyncTaskQueue/tasks/IscoolFetchTask.ts
@@ -0,0 +1,30 @@
+import { IClassesResponse } from '../../../interfaces/class';
+import { IChangesResponse, IScheduleResponse } from '../../../interfaces/lesson';
+import { fetchDataSource } from '../../datasource';
+import { AsyncTask } from '../AsyncTask';
+
+/**
+ * Represents an asyncronous task of fetching the iscool servers
+ * @author Itay Schechner
+ * @version 1.4.0
+ */
+export class IscoolFetchTask extends AsyncTask<IClassesResponse | IScheduleResponse | IChangesResponse, Error> {
+  private params: Parameters<typeof fetchDataSource>;
+  private hasFetched: boolean;
+
+  /**
+   * Initializes a new IscoolFetchTask object.
+   * @param params the parameters of the fetchDataSource object
+   */
+  constructor(...params: Parameters<typeof fetchDataSource>) {
+    super();
+    this.params = params;
+    this.hasFetched = true;
+  }
+
+  protected async beginTask(): Promise<IClassesResponse | IScheduleResponse | IChangesResponse> {
+    const res = await fetchDataSource<IClassesResponse | IScheduleResponse | IChangesResponse>(...this.params);
+    this.hasFetched = true;
+    return res;
+  }
+}
