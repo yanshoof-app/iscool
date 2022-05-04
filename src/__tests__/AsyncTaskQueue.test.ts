41fe6a5751d0fda6c7eaef3373da015072bccc21
itays123 <itays2005@gmail.com>
2022-05-04 16:25:40 +0300

    TEST: AsyncTaskQueue passes

--- src/__tests__/AsyncTaskQueue.test.ts
+++ src/__tests__/AsyncTaskQueue.test.ts
@@ -1,13 +1,19 @@
 import { AsyncTaskQueueTestImpl } from '../utils/AsyncTaskQueue/AsyncTaskQueueTestImpl';
 import { SleepTestTask } from '../utils/AsyncTaskQueue/tasks/SleepTestTask';
-
-const tasks = [100, 200, 300, 400, 500].map((d) => new SleepTestTask(d));
+import { FailTestTask } from '../utils/AsyncTaskQueue/tasks/FailTestTask';
 
 describe('Tests the async task queue method', () => {
   jest.setTimeout(100_000);
+
   let queue: AsyncTaskQueueTestImpl;
-  it('Creates 5 successful tasks and executes them', async () => {
+  let tasks: SleepTestTask[];
+
+  beforeEach(() => {
     queue = new AsyncTaskQueueTestImpl();
+    tasks = [100, 200, 300, 400, 500].map((d) => new SleepTestTask(d));
+  });
+
+  it('Creates 5 successful tasks and executes them', async () => {
     tasks.forEach((task) => queue.enqueue(task));
     expect(queue.size).toBe(tasks.length);
     expect(queue.begunCount).toBe(0);
@@ -16,5 +22,35 @@ describe('Tests the async task queue method', () => {
     expect(queue.size).toBe(0);
     expect(queue.begunCount).toBe(tasks.length);
     expect(queue.errorCount).toBe(0);
+  });
+
+  it('Adds tasks while queue is executing', async () => {
+    queue.enqueue(tasks[0]);
+    queue.enqueue(tasks[1]);
+    queue.enqueue(tasks[2]);
+    tasks[2].on('success', () => {
+      queue.enqueue(tasks[4]);
+      expect(queue.enqueuedCount).toBe(5);
+      expect(queue.begunCount).toBe(3);
+    });
+    queue.enqueue(tasks[3]);
+    expect(queue.enqueuedCount).toBe(4);
+    await queue.execute();
+    expect(queue.begunCount).toBe(5);
+  });
+
+  it('Adds tasks with a problematic one', async () => {
+    queue.enqueue(tasks[0]);
+    queue.enqueue(tasks[1]);
+    queue.enqueue(tasks[2]);
+    queue.enqueue(new FailTestTask(100));
+    queue.enqueue(tasks[3]);
+    queue.enqueue(tasks[4]);
+
+    expect(queue.enqueuedCount).toBe(6);
+    await queue.execute();
+
+    expect(queue.begunCount).toBe(6);
+    expect(queue.errorCount).toBe(1);
   });
 });
